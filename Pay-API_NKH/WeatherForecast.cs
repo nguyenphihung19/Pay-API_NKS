38c708f7bc8a128e8202bf1f2fe2ba390dce8029
Hung <hungnguyen180705@gmail.com>
2026-07-16 20:14:23 +0700

    feat: thiet ke database va cau hinh connection string

---
+++ Pay-API_NKH/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Pay_API_NKH
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
