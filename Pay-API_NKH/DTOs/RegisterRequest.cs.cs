270c8981efd8c61796eeaab5c6c325caf3d4da69
Hung <hungnguyen180705@gmail.com>
2026-07-16 21:37:40 +0700

    feat: hoan thanh API Dang ky tai khoan va cau hinh DB

---
+++ Pay-API_NKH/DTOs/RegisterRequest.cs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pay_API_NKH.DTOs
+{
+    public class RegisterRequest
+    {
+        public string AccountNumber { get; set; } = null!;
+        public string AccountHolder { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string CitizenId { get; set; } = null!;
+        public DateTime ExpiryDate { get; set; }
+        public decimal InitialBalance { get; set; } // Số dư ban đầu người dùng nạp vào
+    }
+}
--- Pay-API_NKH/Program.cs
+++ Pay-API_NKH/Program.cs
@@ -1,7 +1,14 @@
+﻿// 1. TẤT CẢ CÁC DÒNG USING PHẢI ĐẶT Ở TRÊN CÙNG FILE
+using Microsoft.EntityFrameworkCore;
+using Pay_API_NKH.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 2. Cấu hình kết nối SQL Server (Đặt dưới builder)
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
